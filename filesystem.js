7bfdcc447fecd202ea7ae64c0d0d73ef44c92ce3
minjae <minjae@minjaeui-MacBook-Pro.local>
2017-08-18 05:24:33 +0900

    callVision

--- filesystem.js
+++ filesystem.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var path = require('path');
+var _ = require('underscore');
 var vision = require('@google-cloud/vision')({
     projectId: 'Creative Engineering',
     keyFilename: '/Users/minjae/Downloads/Creative Engineering-ae2856be4ceb.json'
+    // "@google-cloud/vision": "^0.11.5"
 });
-var _ = require('underscore');
-const image = '/Users/minjae/git/Dorcus_Server/image';
+const imagePath = '/Users/minjae/git/Dorcus_Server/image';
 
 function getMostRecentFileName(dir) {
+    // read directory file
     var files = fs.readdirSync(dir);
 
     // use underscore for max()
@@ -19,56 +21,24 @@ function getMostRecentFileName(dir) {
     });
 }
 
-console.log(path.join(image, getMostRecentFileName(image)))
-
-
 //이미지 랜딩
-function callVision(imagePath, imageName) {
-
-    vision.detectLabels(imagePath)
+function callVision() {
+    var image = path.join(imagePath, getMostRecentFileName(imagePath))
+    // render label value
+    vision.detectLabels(image)
         .then((results) => {
-        const labels = results[0];
-
-    console.log('Labels:',imageName);
-    labels.forEach((label) => console.log(label));
+    //     const labels = results[0];
+    //
+    // console.log('Labels:',imageName);
+    // labels.forEach((label) => console.log(label));
     console.log(results)
 
     }).catch(err => {
             console.error(err);
     });
 }
-// 가장 최신 파일 탐색
+
+console.log(path.join(imagePath, getMostRecentFileName(imagePath)))
+callVision()
 
 
-
-//path.join(dir,getMostRecentFileName(image))
-//searchForRecentlyFile();
-
-
-/*var searchForRecentlyFile = function () {
-
-    fs.readdir(image, (err, files) => {
-        files.forEach(file => {
-            fs.stat(pathModule.join(image, file), function (err, stats) {
-                if(err) {
-                    throw err;
-                }
-                var time = '';
-                var recentImage = '';
-
-                if (time == '') {
-                    time = stats.birthtime;
-                    recentImage = file
-                }
-                else if (time < stats.birthtime){
-                    time = stats.birthtime;
-                    recentImage = file
-                };
-                //console.log(recent)
-                console.log(time)
-                renderImage(pathModule.join(image, file),recentImage)
-
-            });
-        })
-    })
-};*/
